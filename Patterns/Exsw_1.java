cc6c618dbd70b47a969f9597a63f6c74e4eaaa0f
Saurabh Chavan <68920063+Saurabh3000@users.noreply.github.com>
2024-06-20 21:14:43 +0530

    initial-commit

---
+++ Patterns/Exsw_1.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+import java.util.*;
+
+public class Exsw_1 {
+
+    public static void main(String args[]){
+
+        System.out.print("Enter Friut Name = ");
+        Scanner sc = new Scanner(System.in);
+        String friut = sc.next();
+
+        switch(friut){
+            case "Mango":System.out.println("The King of Fruits");
+            break;
+
+            case "Apple": System.out.println("The Red Sweet Fruit");
+            break;
+
+            default: System.out.println("Invalid Inpute");
+        }
+
+        sc.close();
+
+    }
+
+    
+}
